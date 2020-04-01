e7f3b9a1cdd9868312a41c501549ee19bc9d5234
EVI_BREUKERS <evibreukers@gmail.com>
2020-04-01 16:42:27 +0200

    update april 1

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 // EVENT: toggle menu on hover
-const selectBurger = document.getElementById("burger")
-const selectMenuBox = document.getElementById("menuBox")
-const selectMenu = document.getElementById("menu")
+const selectBurger = document.getElementById('burger')
+const selectMenuBox = document.getElementById('menuBox')
+const selectMenu = document.getElementById('menu')
 
-selectBurger.addEventListener("mouseenter", () => {
-    selectMenu.classList.replace("fadeout", "fadein")
-    selectMenu.style.border = "4px solid white"
+selectBurger.addEventListener('mouseenter', () => {
+    selectMenu.classList.replace('fadeout', 'fadein')
+    selectMenu.style.border = '4px solid white'
 })
 
 selectMenuBox.addEventListener('mouseleave', () => {
-    selectMenu.classList.replace("fadein", "fadeout")
-    setTimeout(function(){
-        if (selectMenu.classList.item(0) === "fadeout")
-        selectMenu.style.border = "none"
+    selectMenu.classList.replace('fadein', 'fadeout')
+    setTimeout(() => {
+        if (selectMenu.classList.item(0) === 'fadeout')
+        selectMenu.style.border = 'none'
     },400)
 })
 
-const backColor = document.getElementById("box").classList
-const selectBtn = document.querySelectorAll("li")
-const selectTxt = document.getElementById("text")
+const backColor = document.getElementById('box').classList
+const selectBtn = document.querySelectorAll('li')
+const selectTxt = document.getElementById('text')
 
 // FUNCTION: change color
 const changeColor = (enterBtn) => {
@@ -32,31 +32,31 @@ const changeColor = (enterBtn) => {
 
     // check current radiobutton
     Array.from(selectBtn).forEach(enterBtn => {
-        enterBtn.firstElementChild.removeAttribute("checked")
-        enterBtn.lastElementChild.classList.remove("checked")
+        enterBtn.firstElementChild.removeAttribute('checked')
+        enterBtn.lastElementChild.classList.remove('checked')
     })
-    enterBtn.firstElementChild.setAttribute("checked","checked")
-    enterBtn.lastElementChild.classList.toggle("checked")
+    enterBtn.firstElementChild.setAttribute('checked','checked')
+    enterBtn.lastElementChild.classList.toggle('checked')
     
     // close menu after click
-    selectMenu.classList.replace("fadein", "fadeout")
-    setTimeout(function(){
-        selectMenu.style.border = "none"
+    selectMenu.classList.replace('fadein', 'fadeout')
+    setTimeout(() => {
+        selectMenu.style.border = 'none'
     }, 400)
 }
 
 // EVENT: click button --> change color 
 selectBtn.forEach(button => {
-    button.addEventListener("click", () => {
+    button.addEventListener('click', () => {
         changeColor(button)
     })
 })
 
 // EVENT: keypress --> change color
-window.addEventListener("keypress", (event) => {
+window.addEventListener('keypress', (event) => {
     let keyCode = event.keyCode
     selectBtn.forEach(button => {
-        let getKey = button.getAttribute("data-key")
+        let getKey = button.getAttribute('data-key')
         if (keyCode == getKey){
             changeColor(button)
         }
